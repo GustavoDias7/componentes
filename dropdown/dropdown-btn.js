b940c7481772c8a8439babc225f1d1792774bb0e
Gustavo <gustavo7dias@gmail.com>
2021-07-27 14:28:01 -0300

    Removing focus event

--- dropdown/dropdown-btn.js
+++ dropdown/dropdown-btn.js
@@ -1,12 +1,7 @@
 function dropdownBTN() {
     const dropdownBTN = document.querySelector('.dropdown-btn');
-    dropdownBTN.addEventListener('click', openDropdownBTN);
-    dropdownBTN.addEventListener('focus', openDropdownBTN);
-    function openDropdownBTN(event) {
-        const eventType = event.type;
-        if (eventType === 'click') {
-            event.currentTarget.classList.toggle('dropdown-btn-open');
-        }
-    }
+    dropdownBTN.addEventListener('click', (event) => {
+        event.currentTarget.classList.toggle('dropdown-btn-open');
+    });
 }
 dropdownBTN();
