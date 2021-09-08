c78350776547ef9459d888472dbe8954759b9436
Gustavo <gustavo7dias@gmail.com>
2021-09-08 17:41:04 -0300

    Update code semantic

--- accordion/accordion.js
+++ accordion/accordion.js
@@ -1,21 +1,26 @@
-function accordion(selectorName, onlyOneOpen = false) {
-  const accordions = document.querySelectorAll(selectorName);
-  let lastOpen = 0
+function accordion({ selectorName, onlyOneOpen = false }) {
+  const $accordions = document.querySelectorAll(selectorName);
+  let lastOpen = 0;
   let setLastOpen = (newValue) => {
     lastOpen = newValue;
   };
 
-  accordions.forEach((accordion, index) => {
+  $accordions.forEach((accordion, index) => {
     accordion.addEventListener('click', (event) => {
       event.currentTarget.toggleAttribute('show');
       if (onlyOneOpen === false) return;
-      let isDifferent = accordions[lastOpen] != accordion;
+      let isDifferent = $accordions[lastOpen] != accordion;
       if (isDifferent) {
-        accordions[lastOpen].removeAttribute('show');
+        $accordions[lastOpen].removeAttribute('show');
         setLastOpen(index);
       }
     })
   })
 }
-accordion('[data-accordion]');
-accordion('[data-only]', true);
+accordion({
+  selectorName: '[data-accordion]',
+});
+accordion({
+  selectorName: '[data-only]',
+  onlyOneOpen: true,
+});
