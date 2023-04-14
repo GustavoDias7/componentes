e4c0897543c6e52a0b460df6c27bfd0cb571113a
Gustavo <gustavo7dias@gmail.com>
2023-04-14 15:12:14 -0300

    Update function to validate multiple fields

--- docs/form-builder/form-builder.js
+++ docs/form-builder/form-builder.js
@@ -1,44 +1,67 @@
-function validation(type) {
-  let value = "";
-  let error = null;
-  let isValid = false;
-  let isTouched = false;
+function validation({ initialValues = {}, initialErrors = {}, schemas }) {
+  const states = {
+    values: { ...initialValues },
+    errors: { ...initialErrors },
+    isValid: {},
+    isTouched: {},
+    isValidForm: false,
+  };
 
-  const setValue = (val) => (value = val);
-  const setError = (val) => (error = val);
-  const setIsValid = (val) => (isValid = val);
-  const setIsTouched = (val) => (isTouched = val);
+  for (const key in states.values) {
+    states.isValid[key] = false;
+    states.isTouched[key] = false;
+  }
+
+  function setValue(name, val) {
+    states.values = { ...states.values, [name]: val };
+  }
+  function setError(name, val) {
+    states.errors = { ...states.errors, [name]: val };
+  }
+  function setIsValid(name, val) {
+    states.isValid = { ...states.isValid, [name]: val };
+  }
+  function setIsTouched(name, val) {
+    states.isTouched = { ...states.isTouched, [name]: val };
+  }
+  function setIsValidForm() {
+    states.isValidForm = Object.entries(states.isValid).every((val) => val[1]);
+  }
 
   function onChange({ target }) {
-    if (isTouched) validate(target.value);
-    setValue(target.value);
+    if (states.isTouched) validate(target.name, target.value);
+    setValue(target.name, target.value);
   }
 
-  function onBlur() {
-    if (!isTouched) setIsTouched(true);
-    return validate(value);
+  function onBlur({ target }) {
+    if (!states.isTouched[target.name]) setIsTouched(target.name, true);
+    return validate(target.name, states.values[target.name]);
   }
 
-  function validate(value) {
-    if (type === false) {
+  function validate(type, value) {
+    if (Boolean(schemas[type]) === false) {
       setIsValid(true);
+      setIsValidForm();
       return true;
     }
 
     if (value.length === 0) {
       // it has no value
-      setError("Preencha um valor");
-      setIsValid(false);
+      setError(type, "Preencha um valor");
+      setIsValid(type, false);
+      setIsValidForm();
       return false;
-    } else if (types[type] && !types[type].validation(value)) {
+    } else if (schemas[type] && !schemas[type].validation(value)) {
       // is not valid
-      setError(types[type].message);
-      setIsValid(false);
+      setError(type, schemas[type].message);
+      setIsValid(type, false);
+      setIsValidForm();
       return false;
     } else {
       // is valid
-      setError(null);
-      setIsValid(true);
+      setError(type, null);
+      setIsValid(type, true);
+      setIsValidForm();
       return true;
     }
   }
@@ -48,17 +71,20 @@ function validation(type) {
     setError,
     setIsValid,
     setIsTouched,
+    setIsValidForm,
     onChange,
     onBlur,
-    value: () => value,
-    isValid: () => isValid,
-    isTouched: () => isTouched,
-    error: () => error,
-    validate: () => validate(value),
+    isValidForm: () => states.isValidForm,
+    values: () => states.values,
+    value: (name) => states.values[name],
+    errors: (name) => states.errors[name],
+    isValid: (name) => states.isValid[name],
+    isTouched: (name) => states.isTouched[name],
+    validate: (type) => validate(type, states.values[type]),
   };
 }
 
-function setErrorMessage(fieldName = "", message, isValid) {
+function setErrorMessage(fieldName = "", message, isValid, classNames) {
   const errorId = `${fieldName}-error-message`;
   const $err = document.querySelector(`#${errorId}`);
   const $field = document.querySelector(`input[name='${fieldName}']`);
@@ -70,7 +96,7 @@ function setErrorMessage(fieldName = "", message, isValid) {
   } else if (!$err && !isValid && message !== null) {
     // create error element
     const $error = document.createElement("p");
-    $error.classList.add("helper-text", "error");
+    $error.classList.add("helper-text", classNames.error);
     $error.innerText = message;
     $error.id = errorId;
     // where to create the error element
@@ -78,101 +104,95 @@ function setErrorMessage(fieldName = "", message, isValid) {
   }
 }
 
-function setClass(fieldName = "", isValid) {
+function setClass(fieldName = "", isValid, classNames) {
   const $field = document.querySelector(`input[name='${fieldName}']`);
   if (isValid) {
-    $field.classList.remove("error");
-    $field.classList.add("success");
+    $field.classList.remove(classNames.error);
+    $field.classList.add(classNames.valid);
   } else {
-    $field.classList.remove("success");
-    $field.classList.add("error");
+    $field.classList.remove(classNames.valid);
+    $field.classList.add(classNames.error);
   }
+}
+
+function setButtonState(isFormValid, formId, formButton) {
+  const $button = document.querySelector(`#${formId} ${formButton}`);
+  $button.disabled = !isFormValid;
 }
 
 function formBuilder({
   initialValues = {},
   initialErrors = {},
   indexToValidate = {},
+  schemas = {},
+  classNames = { error: "error", valid: "success" },
+  formId = "",
+  formButton = "",
+  onFormSubmit = null,
 }) {
-  const $fields = Object.entries(initialValues).map((val) =>
-    document.querySelector(`input[name='${val[0]}']`)
-  );
+  const fields = Object.entries(initialValues).map((val) => val[0]);
+  const {
+    setError,
+    setIsTouched,
+    onChange,
+    onBlur,
+    errors,
+    isValid,
+    isTouched,
+    isValidForm,
+    values,
+  } = validation({ initialValues, schemas });
+  setButtonState(isValidForm(), formId, formButton);
 
-  $fields.forEach(($field) => {
-    const { name: fieldName } = $field;
-    const {
-      setError,
-      setIsTouched,
-      onChange,
-      onBlur,
-      error,
-      isValid,
-      isTouched,
-    } = validation(fieldName);
-
+  fields.forEach((fieldName) => {
     if (initialErrors[fieldName]?.length > 0) {
-      setIsTouched(true);
+      setIsTouched(fieldName, true);
       setError(initialErrors[fieldName]);
-      setErrorMessage(fieldName, initialErrors[fieldName], isValid());
-      setClass(fieldName, isValid());
+      setErrorMessage(
+        fieldName,
+        initialErrors[fieldName],
+        isValid(fieldName),
+        classNames
+      );
+      setButtonState(isValidForm(), formId, formButton);
+      setClass(fieldName, isValid(fieldName), classNames);
     }
 
+    const $field = document.querySelector(`input[name='${fieldName}']`);
+    $field.value = initialValues[fieldName];
+
     $field.addEventListener("input", (event) => {
-      if (event.target.value.length === indexToValidate[fieldName])
-        setIsTouched(true);
+      if (event.target.value.length === indexToValidate[fieldName]) {
+        setIsTouched(fieldName, true);
+      }
       onChange(event);
-      setErrorMessage(fieldName, error(), isValid());
-      if (isTouched()) setClass(fieldName, isValid());
+      setErrorMessage(
+        fieldName,
+        errors(fieldName),
+        isValid(fieldName),
+        classNames
+      );
+      setButtonState(isValidForm(), formId, formButton);
+      if (isTouched(fieldName))
+        setClass(fieldName, isValid(fieldName), classNames);
     });
 
-    $field.addEventListener("blur", () => {
-      onBlur();
-      setErrorMessage(fieldName, error(), isValid());
-      setClass(fieldName, isValid());
+    $field.addEventListener("blur", (event) => {
+      onBlur(event);
+      setErrorMessage(
+        fieldName,
+        errors(fieldName),
+        isValid(fieldName),
+        classNames
+      );
+      setButtonState(isValidForm(), formId, formButton);
+      setClass(fieldName, isValid(fieldName), classNames);
     });
   });
 
-  return {
-    isValidForm: true,
-    onFormSubmit: () => ({ ...initialValues }),
-    validateForm: () => {},
-  };
-}
-
-function handleDigits({
-  fieldName = "",
-  selector = "input.digit",
-  initialError = "",
-}) {
-  const $button = document.querySelector("#validate-digits");
-  const $field = document.querySelector(`input[name='${fieldName}']`);
-  const $digits = document.querySelectorAll(selector);
-  const {
-    setValue,
-    setError,
-    setIsTouched,
-    validate,
-    error,
-    isValid,
-    isTouched,
-  } = validation(fieldName);
-
-  if (initialError) {
-    setIsTouched(true);
-    setError(initialError);
-    setErrorMessage(fieldName, initialError, isValid());
-    setClass(fieldName, isValid());
-  }
-
-  $digits.forEach(($digit) => {
-    $digit.addEventListener("input", () => {
-      setValue($field.value.trim());
-      validate();
-      if (isTouched()) {
-        setErrorMessage(fieldName, error(), isValid());
-        setClass(fieldName, isValid());
-      }
-      $button.disabled = !isValid();
-    });
+  const $form = document.getElementById(formId);
+  $form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (onFormSubmit) onFormSubmit(isValidForm() ? values() : null);
   });
 }
